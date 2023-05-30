eb1cb0d66a7deafb2219871e279816f94ff23596
umeshbisht1 <121707580+umeshbisht1@users.noreply.github.com>
2023-05-30 21:29:12 +0530

    index.py

--- 100day.py
+++ 100day.py
@@ -1,28 +1,4 @@
-#just carr y 
-''''''
-# print("\"umesh is a good boy\"\nand she is also a good:")
-# print("neeraj is tatty gay::")
-# print("just carry on bro::")
-# print("neeraj",7,8,9,10,sep="_",end="234")
-# print("zetty")
-a="hellosingh bisht"
-# for ch in a:
-#     print(ch)
-# for i in range (2,30,3):
-#     print(i)   
-# print(a[-4:-2]) 
-# print(len(a))
-# a=int(input("enter the age:::"))
-# b=int(input("enter the age:::"))
-# print(a,b)
-# temp=a
-# a=b
-# b=temp
-# print(a,b)
-      #triple cote string method::
-# ans='''umesh singh bisht
-# is agood boy 
-# studing in btech 2 year'''
+
 
 import time
 time1=time.strftime('%H:%M:%S')
@@ -39,11 +15,4 @@ elif time2>18 and time2<23 :
 else:
     print("sweet dream sir ")    
 
-# Match Case:;
-# x=4
-# match x:
-#     case 0:
-#          print("umesh")
-#     case _:
-#         print(x)     
 
